e299b14146e46799899db5e162c04463c2739ed5
Georgios Toufexis <georgetouf7@gmail.com>
2024-07-24 21:04:45 -0400

    test

---
+++ Sports-Merchandise-Store/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sports_Merchandise_Store.Startup))]
+namespace Sports_Merchandise_Store
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
