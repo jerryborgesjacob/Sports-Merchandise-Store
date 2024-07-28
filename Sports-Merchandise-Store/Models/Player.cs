04f3183dcb21ef3590b1aaa7411f79494fd84731
Georgios Toufexis <georgetouf7@gmail.com>
2024-07-28 00:41:12 -0400

    Created tables for teams and players

---
+++ Sports-Merchandise-Store/Models/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Passion_Project.Models
+{
+    //Model for holding the player table contents
+    public class Player
+    {
+
+        [Key]
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerPosition { get; set; }
+        public int ShirtNumber { get; set; }
+
+        //foreign key for the player's team
+        [ForeignKey("Team")]
+        //A player can only be playing for one team
+        public int PlayerTeamId { get; set; }
+        public virtual Team Team { get; set; }
+    }
+
+    //DTO section
+    public class PlayerDTO
+    {
+        public int PlayerId { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerPosition { get; set; }
+        public int ShirtNumber { get; set; }
+        public string TeamName { get; set; }
+    }
+}
