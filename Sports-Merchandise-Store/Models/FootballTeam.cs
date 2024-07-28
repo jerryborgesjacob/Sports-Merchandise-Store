e232f9e0dcc94cb8f5e11e9c78db44fcdc65f94c
Georgios Toufexis <georgetouf7@gmail.com>
2024-07-27 23:42:49 -0400

    Added team model

---
+++ Sports-Merchandise-Store/Models/FootballTeam.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+    namespace Sports_Merchandise_Store.Models
+    {
+        //Model for holding the team table contents
+        public class Team
+        {
+            //A team can hold multiple players
+            [Key]
+            public int TeamId { get; set; }
+            public string TeamName { get; set; }
+            public string League { get; set; }
+            public string TeamCountry { get; set; }
+            public decimal TeamBudget { get; set; }
+        }
+
+        //DTO section
+        public class TeamDTO
+        {
+            public int TeamId { get; set; }
+            public string TeamName { get; set; }
+            public string League { get; set; }
+            public string TeamCountry { get; set; }
+            public decimal TeamBudget { get; set; }
+        }
+    }
