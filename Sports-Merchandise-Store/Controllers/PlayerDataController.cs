d9869a5cc5ac9796daf9a2e55f5b076dfa658f39
Georgios Toufexis <georgetouf7@gmail.com>
2024-08-02 22:29:31 -0400

    Added player api controller to get list

---
+++ Sports-Merchandise-Store/Controllers/PlayerDataController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Sports_Merchandise_Store.Models;
+
+/// <info>
+/// API CONTROLLER FOR THE FOOTBALL PLAYERS TABLE
+/// </info>
+
+namespace Sports_Merchandise_Store.Controllers
+{
+    public class PlayerDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        //List Players
+        ///<summary>
+        ///Returns all the players from teh players' table
+        ///</summary>
+        ///<returns>
+        ///The list of players and their info
+        ///</returns>
+        ///<example>
+        ///GET: api/PlayerData/ListPlayers --> (PlayerID, PlayerName, PlayerPosition, ShirtNumber, current Team)
+        ///</example>
+
+        [HttpGet]
+        [Route("api/PlayerData/ListPlayers")]
+        public IHttpActionResult ListPlayers()
+        {
+            List<Player> Players = db.Players.ToList();
+            List<PlayerDTO> PlayerDTOs = new List<PlayerDTO>();
+            Players.ForEach(p => PlayerDTOs.Add(new PlayerDTO
+            {
+                PlayerId = p.PlayerId,
+                PlayerName = p.PlayerName,
+                PlayerPosition = p.PlayerPosition,
+                ShirtNumber = p.ShirtNumber,
+                TeamName = p.Team.TeamName
+            }));
+            return Ok(PlayerDTOs);
+        }
+    }
+}
