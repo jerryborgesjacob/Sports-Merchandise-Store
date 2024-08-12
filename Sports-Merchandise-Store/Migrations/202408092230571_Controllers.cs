28c46b3587385256cbd30dc3f4ee1694a974667e
Jerry Borges Jacob <156135012+jerryborgesjacob@users.noreply.github.com>
2024-08-11 21:41:44 -0400

    F1 migrations added

---
+++ Sports-Merchandise-Store/Migrations/202408092230571_Controllers.cs
@@ -0,0 +1,16 @@
+﻿namespace Sports_Merchandise_Store.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Controllers : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
