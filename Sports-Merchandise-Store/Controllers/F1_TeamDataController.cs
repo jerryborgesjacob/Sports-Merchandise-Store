8df60185950cfd8109531cf02b11daab1a024b41
Jerry Borges Jacob <156135012+jerryborgesjacob@users.noreply.github.com>
2024-08-17 08:34:23 -0400

    add commenting

--- Sports-Merchandise-Store/Controllers/F1_TeamDataController.cs
+++ Sports-Merchandise-Store/Controllers/F1_TeamDataController.cs
@@ -44,6 +44,17 @@ namespace Sports_Merchandise_Store.Controllers
             return F1_TeamDTOs;
         }
 
+        /// <summary>
+        /// Returns the F1 Team details with the specified TeamId
+        /// </summary>
+        /// <param name="id">TeamId of the Team</param>
+        /// <returns>
+        /// HEADER: 200 (Status Code for OK)
+        /// </returns>
+        /// <example>
+        /// // GET: api/F1_TeamData/5 => Data of Team with TeamId 5
+        /// </example>
+
         // GET: api/F1_TeamData/5
         [ResponseType(typeof(F1_Team))]
         public IHttpActionResult GetF1_Team(int id)
@@ -57,7 +68,24 @@ namespace Sports_Merchandise_Store.Controllers
             return Ok(f1_Team);
         }
 
-        // PUT: api/F1_TeamData/5
+        
+        /// <summary>
+        /// Updates the Team details of a particular team with the POST data input
+        /// </summary>
+        /// <param name="id">The TeamId in the table (primary key)</param>
+        /// <param name="team">JSON Form Data of a Team</param>
+        /// <returns>
+        /// Status Code 
+        /// HEADER: 200 = Success
+        /// or
+        /// HEADER: 400 = Bad Request
+        /// or
+        /// HEADER: 404 = Not Found
+        /// </returns>
+        /// <example>
+        /// POST: api/F1_TeamData/UpdateTeam/5
+        /// FORM Data: Team JSON Object
+        /// </example>
         [ResponseType(typeof(void))]
         [HttpPost]
         public IHttpActionResult UpdateF1_Team(int id, F1_Team f1_Team)
@@ -93,7 +121,21 @@ namespace Sports_Merchandise_Store.Controllers
             return StatusCode(HttpStatusCode.NoContent);
         }
 
-        // POST: api/F1_TeamData
+        /// <summary>
+        /// Add New Team details onto the Team table in the Database
+        /// </summary>
+        /// <param name="team">JSON Form Data of the Team</param>
+        /// Status Code 
+        /// HEADER: 200 = Success
+        /// or
+        /// HEADER: 400 = Bad Request
+        /// or
+        /// HEADER: 404 = Not Found
+        /// <example>
+        /// POST: api/F1_TeamData/AddTeam
+        /// FORM Data: Team JSON Object
+        /// </example>
+
         [ResponseType(typeof(F1_Team))]
         [HttpPost]
         public IHttpActionResult AddF1_Team(F1_Team f1_Team)
@@ -109,9 +151,21 @@ namespace Sports_Merchandise_Store.Controllers
             return CreatedAtRoute("DefaultApi", new { id = f1_Team.F1TeamId }, f1_Team);
         }
 
-        // DELETE: api/F1_TeamData/5
+        /// <summary>
+        /// Deletes Team data from the database with TeamId.
+        /// </summary>
+        /// <param name="id">The primary key of the Team</param>
+        /// <returns>
+        /// HEADER: 200 = OK
+        /// or
+        /// HEADER: 404 = Not Found
+        /// </returns>
+        /// <example>
+        /// POST: api/F1_TeamData/DeleteTeam/5
+        /// FORM DATA: (empty)
+        /// </example>
         [ResponseType(typeof(F1_Team))]
-        [Route("api/F1TeamData/DeleteF1Team/{id}")]
+        [Route("api/F1_TeamData/DeleteF1Team/{id}")]
         [HttpPost]
         public IHttpActionResult DeleteF1_Team(int id)
         {
